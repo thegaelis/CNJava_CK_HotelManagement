764fbd9d53943b9b9660a3d53256383b248d9415
thaihq <112237841+thegaelis@users.noreply.github.com>
2023-10-06 10:48:54 +0700

    Init Project
    Intial base files

---
+++ README.md
@@ -0,0 +1,3 @@
+Đồ án cuối kỳ
+Môn: Công nghệ Java
+Đề tài: Xây dựng web quản lý khách sạn
---
+++ Tutorial/Basic1/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+            Scanner x = new Scanner(System.in);
+            int n;
+            System.out.print("Nhap n: ");
+            n= x.nextInt();
+            System.out.println(n);
+            for (int i=0;i<n;i++) {
+                System.out.print(i+" ");
+            }
+
+            String v = "Hello dear my puissant";
+        }
+    }
